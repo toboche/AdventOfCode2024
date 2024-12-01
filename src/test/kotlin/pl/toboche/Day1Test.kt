e23dcd6103de87626a99b3109bbffc0f4d682d12
toboche <toboche@gmail.com>
2024-12-01 11:49:01 +0100

    Day  1 task 2 example + solution

--- src/test/kotlin/pl/toboche/Day1Test.kt
+++ src/test/kotlin/pl/toboche/Day1Test.kt
@@ -32,4 +32,30 @@ internal class Day1Test {
         )
     }
 
+    @Test
+    internal fun task2Example() {
+        val input = ("3   4\n" +
+                "4   3\n" +
+                "2   5\n" +
+                "1   3\n" +
+                "3   9\n" +
+                "3   3")
+            .lines()
+        assertEquals(
+            31,
+            Day1().task2(input)
+        )
+    }
+
+    @Test
+    internal fun task2() {
+        val input = File("src/test/resources/day1Task1.txt")
+            .readText()
+            .lines()
+
+        assertEquals(
+            23117829,
+            Day1().task2(input)
+        )
+    }
 }
