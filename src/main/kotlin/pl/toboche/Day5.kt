de0457f17f3ddb348a9f6ec10df64bb872adeec9
toboche <toboche@gmail.com>
2024-12-05 22:13:35 +0100

    Day 5 task 2

--- src/main/kotlin/pl/toboche/Day5.kt
+++ src/main/kotlin/pl/toboche/Day5.kt
@@ -1,5 +1,7 @@
 package pl.toboche
 
+import java.util.*
+
 class Day5 {
     fun task1(input: List<String>): Int {
         val rulesEnd = input.indexOfFirst { it.isBlank() }
@@ -29,19 +31,41 @@ class Day5 {
     }
 
     fun task2(input: List<String>): Int {
-        val left = mutableListOf<Int>()
-        val right = mutableListOf<Int>()
-        input.forEach() {
-            val (l, r) = it.split("   ").map { it.toInt() }
-            left.add(l)
-            right.add(r)
+        val rulesEnd = input.indexOfFirst { it.isBlank() }
+        val rules = input.take(rulesEnd)
+            .map {
+                val (a, b) = it.split('|')
+                a.toInt() to b.toInt()
+            }.groupBy(keySelector = { it.first }, valueTransform = { it.second })
+        val updates = input.drop(rulesEnd + 1)
+            .map { it.split(',').map { it.toInt() } }
+        return updates.filterNot { update ->
+            update.foldIndexed(true) { currentIndexInUpdate, acc, pageToUpdate ->
+                val pagesRequiredAfter = rules.getOrDefault(pageToUpdate, emptyList())
+                acc && pagesRequiredAfter.fold(true) { accForSinglePageAfter, pageRequiredAfter ->
+                    accForSinglePageAfter && (update.indexOf(
+                        pageRequiredAfter
+                    ).let {
+                        if (it == -1) {
+                            Int.MAX_VALUE
+                        } else {
+                            it
+                        }
+                    } > currentIndexInUpdate)
+                }
+            }
         }
-        left.sort()
-        val rightCounts = right.withCounts()
-        return left.indices.sumOf {
-            left[it] * ((rightCounts[left[it]]) ?: 0)
+            .map { update ->
+                val set = TreeSet<Int> { l, r ->
+                    if (!rules.contains(l)) -1
+                    else if (rules[l]!!.contains(r)) 1
+                    else -1
+                }
+                update.forEach { set.add(it) }
+                set.toList()
+            }
 
-        }
+            .sumOf { it[it.size / 2] }
     }
 
 }
--- src/test/kotlin/pl/toboche/Day5Test.kt
+++ src/test/kotlin/pl/toboche/Day5Test.kt
@@ -49,8 +49,57 @@ internal class Day5Test {
             .lines()
 
         assertEquals(
-            2756096,
+            4609,
             Day5().task1(input)
+        )
+    }
+
+    @Test
+    internal fun task2Example() {
+        val input = ("47|53\n" +
+                "97|13\n" +
+                "97|61\n" +
+                "97|47\n" +
+                "75|29\n" +
+                "61|13\n" +
+                "75|53\n" +
+                "29|13\n" +
+                "97|29\n" +
+                "53|29\n" +
+                "61|53\n" +
+                "97|53\n" +
+                "61|29\n" +
+                "47|13\n" +
+                "75|47\n" +
+                "97|75\n" +
+                "47|61\n" +
+                "75|61\n" +
+                "47|29\n" +
+                "75|13\n" +
+                "53|13\n" +
+                "\n" +
+                "75,47,61,53,29\n" +
+                "97,61,53,29,13\n" +
+                "75,29,13\n" +
+                "75,97,47,61,53\n" +
+                "61,13,29\n" +
+                "97,13,75,29,47")
+            .lines()
+        assertEquals(
+            123,
+            Day5().task2(input)
+        )
+    }
+
+    @Test
+    internal fun task2() {
+        val input = File("src/test/resources/day5Task1.txt")
+            .readText()
+            .lines()
+
+        assertEquals(
+            4609,
+            Day5().task2(input)
         )
     }
 }
