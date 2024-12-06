50141a23c45661f706e728a6bf9fb43573873adc
toboche <toboche@gmail.com>
2024-12-06 07:27:38 +0100

    Day 6 task 1

--- src/test/kotlin/pl/toboche/Day6Test.kt
+++ src/test/kotlin/pl/toboche/Day6Test.kt
@@ -2,6 +2,7 @@ package pl.toboche
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class Day6Test {
     @Test
@@ -19,6 +20,18 @@ internal class Day6Test {
             .lines()
         assertEquals(
             41,
+            Day6().task1(input)
+        )
+    }
+
+    @Test
+    internal fun task1() {
+        val input = File("src/test/resources/day6Task1.txt")
+            .readText()
+            .lines()
+
+        assertEquals(
+            4826,
             Day6().task1(input)
         )
     }
