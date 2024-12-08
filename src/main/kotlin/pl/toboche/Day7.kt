147c741772f37e71788d6f471759fc846d4cbd38
toboche <toboche@gmail.com>
2024-12-08 08:20:46 +0100

    Day 7 task 2 example

--- src/main/kotlin/pl/toboche/Day7.kt
+++ src/main/kotlin/pl/toboche/Day7.kt
@@ -2,24 +2,36 @@ package pl.toboche
 
 class Day7 {
     fun task1(input: List<String>): Long {
-        return input.sumOf {
-            val (expectedResultString, valuesString) = it.split(": ")
-            val expectedResult = expectedResultString.toLong()
-            val values = valuesString.split(' ').map { it.toLong() }
-            var possibleOutcomes = listOf(values.first())
-            for (value in values.drop(1)) {
-                possibleOutcomes = possibleOutcomes.flatMap { possibleResult ->
-                    listOf(
-                        possibleResult + value,
-                        possibleResult * value
-                    )
+        return solve(input, listOf(
+            { a, b -> a + b },
+            { a, b -> a * b }
+        ))
+    }
+
+    fun task2(input: List<String>): Long {
+        return solve(input, listOf(
+            { a, b -> a + b },
+            { a, b -> a * b },
+            { a, b -> (a.toString() + b.toString()).toLong() }
+        ))
+    }
+
+    private fun solve(input: List<String>, ops: List<(Long, Long) -> Long>) = input.sumOf {
+        val (expectedResultString, valuesString) = it.split(": ")
+        val expectedResult = expectedResultString.toLong()
+        val values = valuesString.split(' ').map { it.toLong() }
+        var possibleOutcomes = listOf(values.first())
+        for (value in values.drop(1)) {
+            possibleOutcomes = possibleOutcomes.flatMap { possibleResult ->
+                ops.map { op ->
+                    op(possibleResult, value)
                 }
             }
-            if (possibleOutcomes.contains(expectedResult)) {
-                expectedResult
-            } else {
-                0
-            }
+        }
+        if (possibleOutcomes.contains(expectedResult)) {
+            expectedResult
+        } else {
+            0
         }
     }
 
--- src/test/kotlin/pl/toboche/Day7Test.kt
+++ src/test/kotlin/pl/toboche/Day7Test.kt
@@ -30,8 +30,26 @@ internal class Day7Test {
             .lines()
 
         assertEquals(
-            2756096,
+            1289579105366,
             Day7().task1(input)
+        )
+    }
+
+    @Test
+    internal fun task2Example() {
+        val input = ("190: 10 19\n" +
+                "3267: 81 40 27\n" +
+                "83: 17 5\n" +
+                "156: 15 6\n" +
+                "7290: 6 8 6 15\n" +
+                "161011: 16 10 13\n" +
+                "192: 17 8 14\n" +
+                "21037: 9 7 18 13\n" +
+                "292: 11 6 16 20")
+            .lines()
+        assertEquals(
+            11387,
+            Day7().task2(input)
         )
     }
 }
