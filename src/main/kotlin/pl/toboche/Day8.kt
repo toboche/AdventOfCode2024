691bbd7fca68c34b4939bc69edcc58b0c38c6295
toboche <toboche@gmail.com>
2024-12-08 09:12:43 +0100

    Day 8 task 1

--- src/main/kotlin/pl/toboche/Day8.kt
+++ src/main/kotlin/pl/toboche/Day8.kt
@@ -1,16 +1,14 @@
 package pl.toboche
 
 class Day8 {
-    fun task1(input: List<String>): Int {
+    fun task1(input: List<String>, print: Boolean = false): Int {
         val antennaTypeToAntennaLocations: MutableMap<Char, List<Pair<Int, Int>>> = mutableMapOf()
 
         input.forEachIndexed { j, row ->
             row.forEachIndexed { i, char ->
-                run {
-                    if (char != '.') {
-                        antennaTypeToAntennaLocations[char] =
-                            antennaTypeToAntennaLocations.getOrDefault(char, emptyList()) + (i to j)
-                    }
+                if (char != '.') {
+                    antennaTypeToAntennaLocations[char] =
+                        antennaTypeToAntennaLocations.getOrDefault(char, emptyList()) + (i to j)
                 }
             }
         }
@@ -22,14 +20,31 @@ class Day8 {
                     if (first == second || first.first > second.first) {
                         continue
                     }
-                    val diff = first.first - second.first to first.second - second.second
-                    allAntinodes.add(second.first - diff.first to second.second - diff.second)
-                    allAntinodes.add(first.first + diff.second to first.second + diff.first)
+                    val diff = second.first - first.first to second.second - first.second
+                    allAntinodes.add(first.first - diff.first to first.second - diff.second)
+                    allAntinodes.add(second.first + diff.first to second.second + diff.second)
                 }
             }
         }
         val filtered = allAntinodes
             .filter { it.first in input.first().indices && it.second in input.indices }
+
+        if (print) {
+            input.forEachIndexed { j, row ->
+                row.forEachIndexed { i, char ->
+                    if (filtered.contains(i to j)) {
+                        print('#')
+                    } else
+                        if (char != '.') {
+                            print(char)
+                        } else {
+                            print('.')
+                        }
+                }
+                print('\n')
+            }
+        }
+
         return filtered
             .count()
     }
--- src/test/kotlin/pl/toboche/Day8Test.kt
+++ src/test/kotlin/pl/toboche/Day8Test.kt
@@ -2,6 +2,7 @@ package pl.toboche
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class Day8Test {
     @Test
@@ -40,6 +41,18 @@ internal class Day8Test {
             .lines()
         assertEquals(
             2,
+            Day8().task1(input)
+        )
+    }
+
+    @Test
+    internal fun task1() {
+        val input = File("src/test/resources/day8Task1.txt")
+            .readText()
+            .lines()
+
+        assertEquals(
+            2756096,
             Day8().task1(input)
         )
     }
