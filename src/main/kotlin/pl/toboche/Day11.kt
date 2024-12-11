44e48ff210fdb6783fc87fb3b64b87987b728ba1
toboche <toboche@gmail.com>
2024-12-11 18:51:35 +0100

    Day 11 task 2

--- src/main/kotlin/pl/toboche/Day11.kt
+++ src/main/kotlin/pl/toboche/Day11.kt
@@ -1,29 +1,6 @@
 package pl.toboche
 
 class Day11 {
-//    fun task1(input: String): Int {
-//
-//        return input.split(" ")
-//            .map { it.toLong() }
-//            .map { number ->
-//                (0 until 25).fold(listOf(number)) { acc, i ->
-//                    acc.flatMap { cur ->
-//                        if (cur == 0L) {
-//                            listOf(1L)
-//                        } else {
-//                            val str = cur.toString()
-//                            if (str.count() % 2 == 0) {
-//                                listOf(str.take(str.count() / 2).toLong(), str.takeLast(str.count() / 2).toLong())
-//                            } else {
-//                                listOf(2024 * cur)
-//                            }
-//                        }
-//                    }
-//                }.count()
-//            }
-//            .sum()
-//    }
-
 
     fun task1(input: String, count: Int = 25): Long {
         return input.split(" ")
